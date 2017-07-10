a45e665df8b2c7e17637e13c3bc8b83994616598
karanbhagat <cv.ryn7@gmail.com>
2017-07-10 16:35:38 -0400

    Setup the node server to listen at 3000. Fixed body-parser module import type issue

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongosse = require('mongoose');
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, ()=> {
+    console.log('Server started on port ' + port);
+});
