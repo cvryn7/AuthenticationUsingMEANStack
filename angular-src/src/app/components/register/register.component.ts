c396b53e10f624ad5e8a4f05f1524718cd6bed9c
karanbhagat <cv.ryn7@gmail.com>
2017-07-12 08:27:54 -0400

    Completed the registration implementation. Now register component is capable to using auth service to call nodejs for registration

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ValidateService} from '../../services/validate.service';
 import {FlashMessagesService} from 'angular2-flash-messages';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -15,7 +17,10 @@ export class RegisterComponent implements OnInit {
   password: String;
 
   //Inject service into constructor using parameter
-  constructor(private validateService: ValidateService, private flashMessage: FlashMessagesService) { }
+  constructor(private validateService: ValidateService,
+              private flashMessage: FlashMessagesService,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
   }
@@ -38,5 +43,16 @@ export class RegisterComponent implements OnInit {
       this.flashMessage.show('Please use a valid email', {cssClass: 'alert-danger', timeout: 3000});
       return false;
     }
+
+    // Register user
+    this.authService.registerUser(user).subscribe(data => {
+      if (data.success) {
+        this.flashMessage.show('You are now registered and can log in', {cssClass: 'alert-success', timeout: 3000});
+        this.router.navigate(['/login']);
+      } else {
+        this.flashMessage.show('Something went wrong', {cssClass: 'alert-danger', timeout: 3000});
+        this.router.navigate(['/register']);
+      }
+    });
   }
 }
---
+++ angular-src/src/app/services/auth.service.ts
@@ -0,0 +1,24 @@
+/**
+ * Service for registering user. This service sends requests to express backend
+ */
+import { Injectable } from '@angular/core';
+import { Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class AuthService {
+  authToken: any;
+  user: any;
+
+  constructor(private http: Http) { }
+
+  registerUser(user) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    //return a observable
+    return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
+      .map(res => res.json());
+  }
+
+}
